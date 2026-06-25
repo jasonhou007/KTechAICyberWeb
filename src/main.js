4bdf9c44c8fc5d1741d31f031216fe9943aef4d0
DevAgent <devagent@ktech.fintech>
2026-06-25 15:42:47 +0800

    Phase 2 Round 2417

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
