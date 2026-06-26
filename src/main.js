f42c834aefd4f28522c662abeeb5202962483670
jinbo.h <jinbo.h@kaitaitech.cn>
2026-06-26 22:57:57 +0800

    #49 Create Mobile App service detail page with E2E tests

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import Home from './views/Home.vue'
 import About from './views/About.vue'
 import News from './views/News.vue'
 import NewsDetail from './views/NewsDetail.vue'
+import JoinUs from './views/JoinUs.vue'
+import MobileApp from './views/MobileApp.vue'
+import Blockchain from './views/Blockchain.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
   { path: '/news', component: News },
-  { path: '/news/:slug', component: NewsDetail, props: true }
+  { path: '/news/:slug', component: NewsDetail, props: true },
+  { path: '/join-us', component: JoinUs },
+  { path: '/services/mobile-app', component: MobileApp },
+  { path: '/services/blockchain', component: Blockchain }
 ]
 
 const router = createRouter({
